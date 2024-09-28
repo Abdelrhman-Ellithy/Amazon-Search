e6861eea03ca5354cada4897654f2c1a3a9125e7
Abdelrhman-Ellithy <https://github.com/Abdelrhman-Ellithy>
2024-09-28 16:00:08 +0300

    Update after the release

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -7,5 +7,5 @@ import io.cucumber.testng.CucumberOptions;
         glue = "stepDefinitions", // path to your stepDefinitions package, note you should use . instead of /
         features="src/test/resources/features" // path to your features folder
 )
-public class TestRunner extends SETUP {
+public class TestRunner extends BDDSetup {
 }
